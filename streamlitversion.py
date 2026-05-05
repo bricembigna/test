ecab4941387a510efd350864bff3b2b60e1455b6
fabianpeyer11-png <fabianpeyer11@gmail.com>
2026-05-05 18:00:10 +0200

    Update streamlitversion.py

--- streamlitversion.py
+++ streamlitversion.py
@@ -43,7 +43,7 @@ if st.session_state.page == "Home":
     # The Home page introduces the football analytics application
     # and serves as the main navigation hub.
 
-    st.title("⚽ Football Club Performance MOnitor")
+    st.title("⚽ Football Club Performance Monitor")
 
     st.write(
         "This application provides a comprehensive overview of player performance, "
